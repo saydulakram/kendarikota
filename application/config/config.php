3da0b39ddd80fd2745760c3340b2735c11675c7f
saydulakram <saydulakram@gmail.com>
2021-08-13 15:04:25 +0800

    update base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set("Asia/makassar");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/kendarikota';
+$config['base_url'] = 'http://localhost/kendarikota';
 
 /*
 |--------------------------------------------------------------------------
